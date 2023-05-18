be867e27d63a01be908320119db91158352966af
ornel77 <t.ornel@yahoo.fr>
2023-05-18 18:27:23 +0200

    PUT for users OK

--- app.js
+++ app.js
@@ -38,6 +38,9 @@ app.get("/api/users/:id", userHandlers.getUserById)
 // POST
 app.post('/api/users', userHandlers.postUsers)
 
+// PUT
+app.put('/api/users/:id', userHandlers.updateUser)
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
